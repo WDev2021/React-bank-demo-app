fd331974a5f4b0cfffd031b3c1e2f6245718b274
Jaime <salamanca10@me.com>
2021-08-29 17:39:10 -0400

    table format

--- alldata.js
+++ alldata.js
@@ -18,34 +18,57 @@ function AllData(){
       <h5>All Data in Store</h5>
       {/* {JSON.stringify(ctx)}<br/> */}
 
-      <div className="card text-white mb-3" style={{maxWidth: "27rem"}}>
+      <table class="table table-striped">
+        <thead class="table-dark">
+          <tr class="bg-primary">
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Password</th>
+            <th scope="col">Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersList.map((user, i) => (
+            <tr key={i}>
+              <th scope="row">{i}</th>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+              <td>${user.balance}</td>
+            </tr>
+          ))}
+          </tbody>
+      </table>
+
+      {/* <div className="card text-white mb-3">
         <div className="card-body">
           <div className="row d-flex justify-content-center text-center">
             <table className="table table-bordered table-responsive" >
               <thead>
                 <tr>
-                  <th className="bg-info" scope="col">#</th>
-                  <th className="bg-info" scope="col">Name</th>
-                  <th className="bg-info" scope="col">Email</th>
-                  <th className="bg-info" scope="col">Password</th>
-                  <th className="bg-info" scope="col">Balance</th>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Password</th>
+                  <th scope="col">Balance</th>
                 </tr>
               </thead>
               <tbody>
                 {usersList.map((user, i) => (
                   <tr key={i}>
-                    <th className="bg-info" scope="row">{i}</th>
-                    <td className="bg-info">{user.name}</td>
-                    <td className="bg-info">{user.email}</td>
-                    <td className="bg-info">{user.password}</td>
-                    <td className="bg-info">${user.balance}</td>
+                    <th scope="row">{i}</th>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.password}</td>
+                    <td>${user.balance}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
